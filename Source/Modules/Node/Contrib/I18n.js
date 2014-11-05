38b6e73780a6cd0c4772669c1e03b93d96909b32
Xeriab Nabil <kodeburner@gmail.com>
2014-11-05 05:37:48 +0300

    Added for later use

---
+++ Source/Modules/Node/Contrib/I18n.js
@@ -0,0 +1,26 @@
+/**
+ * YAX I18N [DOM/NODE]
+ */
+
+/*jslint indent: 4 */
+/*jslint browser: true */
+/*jslint white: true */
+/*jshint -W084 */
+/*jslint node: false */
+/*global YAX, Y */
+
+(function () {
+
+	//---
+
+	'use strict';
+
+	
+
+	//---
+
+}());
+
+// FILE: ./Source/Modules/Node/Contrib/I18n.js
+
+//---
