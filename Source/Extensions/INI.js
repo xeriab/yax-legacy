7e84a146406cde01c0701263143fc077f52db37c
Xeriab Nabil <kodeburner@gmail.com>
2014-11-05 05:21:30 +0300

    Added for later use

---
+++ Source/Extensions/INI.js
@@ -0,0 +1,37 @@
+/**
+ * YAX INI Parser [CORE][DOM/NODE][EXTENSION]
+ */
+
+/*jslint indent: 4 */
+/*jslint white: true */
+/*jshint eqeqeq: false */
+/*jshint strict: false */
+/*global Y, YAX */
+
+(function () {
+
+	//---
+
+	'use strict';
+
+	Y.Parser.mergeObject('drivers', {
+		'INI': {
+			id: 'Y.Parser.drivers.INI',
+
+			encode: function (object, option) {
+
+			},
+
+			decode: function (string) {
+
+			}
+		}
+	});
+
+	//---
+
+}());
+
+// FILE: ./Source/Extensions/LocalStorage.js
+
+//---
