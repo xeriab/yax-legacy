49e905c3ca5d9176326a77a52477e2b973a9bad4
Xeriab Nabil <kodeburner@gmail.com>
2014-11-05 05:23:51 +0300

    Added for later use for now

--- Examples/nodejs-1.js
+++ Examples/nodejs-1.js
@@ -24,25 +24,31 @@ require('./../Source/Core/Evented');
 
 require('./../Source/Core/Contrib/Callbacks');
 require('./../Source/Core/Contrib/Deferred');
+require('./../Source/Core/Contrib/Config');
 require('./../Source/Core/Contrib/Store');
 require('./../Source/Core/Contrib/Parser');
 require('./../Source/Core/Contrib/I18n');
 
 //require('./Modules/EnvDetector');
 
-//Y.define('Lumlim', function (require, exports, module) {
-//	var Lumlim = {
-//		VERSION: '0.1.9',
-//		NAME: 'App'
-//	};
-//
-//	module.exports = Lumlim;
-//
-//	global.Lumlim = Lumlim;
-//
-//	return Lumlim;
-//});
-//
-//Y.require('Lumlim');
+// Y.define('Lumlim', [require, exports, module], function () {
+Y.define('Lumlim', [require, exports, module], function () {
+	var Lumlim = {
+		VERSION: '0.1.9',
+		NAME: 'App'
+	};
+
+	// module.exports = Lumlim;
+	// exports = Lumlim;
+
+	// global.Lumlim = Lumlim;
+
+	// return Lumlim;
+	Y.LOG(e.toString());
+});
+
+var Lumlim = Y.require('Lumlim');
 
 //Y.LOG(Y.keys(Y));
+
+Y.LOG(Lumlim);
