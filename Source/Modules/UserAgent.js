cea8813c95e3937584cd1a60b91bacdebfba3f09
Xeriab Nabil <kodeburner@gmail.com>
2014-11-05 05:29:50 +0300

    Added to be used later

---
+++ Source/Modules/UserAgent.js
@@ -0,0 +1,25 @@
+/**
+ * YAX Env./System/UserAgent Detector [DOM/NODE][CORE][MODULE]
+ */
+
+/*jslint indent: 4 */
+/*jslint white: true */
+/*jshint eqeqeq: false */
+/*jshint strict: false */
+/*global Y, YAX, module */
+
+(function () {
+
+	//---
+
+	'use strict';
+
+	
+
+	//---
+
+}());
+
+// FILE: ./Source/Modules/UserAgent.js
+
+//---
